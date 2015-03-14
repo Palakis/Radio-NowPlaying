ad4d9e3076f0fccdf035b9c022ddc13dd6648307
Stéphane Lepin <contact@stephanelep.in>
2015-03-14 15:54:07 +0100

    Added more comments and removed Last.fm cover art integration

--- lib/OnAir.php
+++ lib/OnAir.php
@@ -1,49 +1,55 @@
 <?php
 class OnAir {
-	private $config;
+	private $config; // Self-explanatory
 
-	private $platforms;
-	private $coverProvider;
-	private $meta;
+	private $platforms; // Metadata destinations
+	private $coverProvider; // Cover art providers
+	private $meta; // Metadata to send
+
+	private $allowedHosts; // Hosts allowed to send metadata
+	private $textFilters; // Regexes applied to text values
 
 	public function __construct(array $config) {
+		// Load config in this object
+		// and instanciate platform classes and cover art providers
 		$this->loadConfig($config);
 
+		// POST check
 		if(isset($this->config['forcePost'])
 		   && $this->config['forcePost']
 		   && $_SERVER['REQUEST_METHOD'] != "POST") {
 			throw new Exception('Unsupported method');
 		}
 
+		// "title" attribute presence check for type "Music"
 		if($_REQUEST['type'] == 'Music' && !isset($_REQUEST['title'])) {
 			throw new Exception("Missing parameters for type Music");
 		}
 
+		// Presence check for common parameters
 		if(!isset($_REQUEST['artist'])
 		|| !isset($_REQUEST['type'])
 		|| !isset($_REQUEST['duration'])) {
 			throw new Exception("Missing parameters");
 		}
-			
-		$this->allowedHosts = array();
-		$this->platforms = array();
-		$this->textFilters = array();
 
+		// Init metadata values
 		$this->meta = new Metadata();
 		$this->meta->Artist = $this->filter($_REQUEST['artist']);
 		$this->meta->Type = $this->filter($_REQUEST['type']);
+		$this->meta->Duration = intval($this->filter($_REQUEST['duration']));
 
+		// Generate a oneliner
 		$this->meta->Oneliner = $this->meta->Artist;
 		if(isset($_REQUEST['title'])) {
 			$this->meta->Title = $this->filter($_REQUEST['title']);
 			$this->meta->Oneliner .= " - ".$this->meta->Title;
 		}
-
-		$this->meta->Duration = intval($this->filter($_REQUEST['duration']));
 	}	
 
 	public function loadConfig(array $config) {
 		$this->config = $config;
+		$this->platforms = array();
 
 		try {
 			$class = 'CoverArt_'.$config['coverArtProvider'];
--- onair.php
+++ onair.php
@@ -1,6 +1,7 @@
 <?php
 mb_internal_encoding("UTF-8");
 
+// Register class autoloader
 spl_autoload_register(function($className) {
 	$filename = str_replace('_', DIRECTORY_SEPARATOR, $className);
 	$filename = 'lib/'.$filename.'.php';
@@ -9,14 +10,17 @@ spl_autoload_register(function($className) {
 	}
 });
 
+// Include config file
 require_once 'config.php';
 
 try {
-	// Do not touch this
+	// Create the core object, give it the configuration array 
+	// and launch it
 	$onair = new OnAir($config);
 	$onair->run();
 }
 catch(Exception $ex) {
+	// Catch any error, log it to the errors.log file, and print it
 	OnAir::Log($ex->getMessage());
 }
 
