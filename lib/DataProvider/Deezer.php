7af6829d1d70f2d576bc9ad60ff2a1cecf424008
Stéphane Lepin <contact@slepin.fr>
2016-05-31 15:27:52 +0200

    Fixed bad indenting

--- lib/DataProvider/Deezer.php
+++ lib/DataProvider/Deezer.php
@@ -1,40 +1,40 @@
 <?php
 class DataProvider_Deezer implements DataProviderInterface {
 	protected $endpoint = "http://api.deezer.com/2.0/";
-        protected $artist;
-        protected $title;
+  protected $artist;
+  protected $title;
 
 	public function __construct($artist, $title) {
-                $this->artist = $artist;
-                $this->title = $title;
+    $this->artist = $artist;
+    $this->title = $title;
 	}
 
-        public function getCover() {
+  public function getCover() {
 		$artist = $this->artist;
 		$title = $this->title;
 
-                $url = $this->endpoint;
+    $url = $this->endpoint;
 
-                $query = urlencode($artist." ".$title);
+    $query = urlencode($artist." ".$title);
 
-                $get = curl_init();
-                curl_setopt($get, CURLOPT_URL, $url."search?q=".$query);
-                curl_setopt($get, CURLOPT_HEADER, FALSE);
-                curl_setopt($get, CURLOPT_RETURNTRANSFER, TRUE);
+    $get = curl_init();
+    curl_setopt($get, CURLOPT_URL, $url."search?q=".$query);
+    curl_setopt($get, CURLOPT_HEADER, FALSE);
+    curl_setopt($get, CURLOPT_RETURNTRANSFER, TRUE);
 
-                $result = curl_exec($get);
-                curl_close($get);
+    $result = curl_exec($get);
+    curl_close($get);
 
-                $json = json_decode($result);
-                if($json->total > 0) {
-                        return $json->data[0]->album->cover."?size=medium";
-                } else {
-                        throw new Exception('No cover art found');
-                }
-        }
+    $json = json_decode($result);
+    if($json->total > 0) {
+      return $json->data[0]->album->cover."?size=medium";
+    } else {
+    	throw new Exception('No cover art found');
+    }
+	}
 
-        public function getPreview() {
-                throw new Exception('Feature not implemented');
-        }
+  public function getPreview() {
+    throw new Exception('Feature not implemented');
+  }
 }
 ?>
--- lib/DataProvider/Spotify.php
+++ lib/DataProvider/Spotify.php
@@ -4,34 +4,34 @@ class DataProvider_Spotify implements DataProviderInterface {
 	protected $track;
 
 	public function __construct($artist, $title) {
-                $this->track = $this->getTrack($artist, $title);
+  	$this->track = $this->getTrack($artist, $title);
 	}
 
-        public function getCover() {
-                if($this->track != null) {
-                        return $this->track['album']['images'][1]['url'];
-                } else {
-                        throw new Exception('No cover art found');
-                }
-        }
+  public function getCover() {
+  	if($this->track != null) {
+      return $this->track['album']['images'][1]['url'];
+    } else {
+      throw new Exception('No cover art found');
+    }
+  }
 
-        public function getPreview() {
-                if($this->track != null) {
-                        return $this->track['preview_url'];
-                } else {
-                        throw new Exception('No preview found');
-                }
-        }
+  public function getPreview() {
+    if($this->track != null) {
+    	return $this->track['preview_url'];
+    } else {
+  		throw new Exception('No preview found');
+    }
+  }
 
-        protected function getTrack($artist, $title) {
-                $url = $this->endpoint;
+  protected function getTrack($artist, $title) {
+    $url = $this->endpoint;
 
-                $query = urlencode($artist." ".$title);
+    $query = urlencode($artist." ".$title);
 
-                $data = SimpleHTTP::get($this->endpoint."/v1/search?type=track&q=".$query);
-                $data = json_decode($data, true);
+    $data = SimpleHTTP::get($this->endpoint."/v1/search?type=track&q=".$query);
+    $data = json_decode($data, true);
 
-                return $data['tracks']['items'][0];
-        }
+    return $data['tracks']['items'][0];
+  }
 }
 ?>
