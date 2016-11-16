84f4bdc84d3e268995366102aa5500097d023d2e
Palakis <contact@slepin.fr>
2016-11-16 14:17:40 +0100

    Switched to LGPL 3.0

--- lib/Platform/TuneIn.php
+++ lib/Platform/TuneIn.php
@@ -1,4 +1,22 @@
 <?php
+/*
+Radio-NowPlaying	
+Copyright (C) 2016 Stéphane Lepin <stephane.lepin@gmail.com>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3.0 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+	
+You should have received a copy of the GNU Lesser General Public
+License along with this library. If not, see <https://www.gnu.org/licenses/>
+*/
+
 class Platform_TuneIn implements PlatformInterface {
 	private $partnerId;
 	private $partnerKey;
@@ -21,4 +39,5 @@ class Platform_TuneIn implements PlatformInterface {
 		SimpleHTTP::get($url);
 	}
 }
+
 ?>
