4e82cf394d80041140e26473bac26ad5303e4c70
Stéphane Lepin <stephane.lepin@gmail.com>
2019-08-02 22:15:40 +0200

    SimpleHTTP: add query params to GET()

--- lib/SimpleHTTP.php
+++ lib/SimpleHTTP.php
@@ -18,7 +18,7 @@ License along with this library. If not, see <https://www.gnu.org/licenses/>
 */
 
 class SimpleHTTP {
-	public static function get($url, $auth = null, 
+	public static function get($url, $params = null, $auth = null, 
 		array $headers = null, $useragent = "NowPlaying(Mozilla Compatible)"
 	) {
 		$curl = curl_init();
@@ -32,17 +32,23 @@ class SimpleHTTP {
 			curl_setopt($curl, CURLOPT_USERPWD, $auth["username"].":".$auth["password"]);
 		}
 
-		if($headers != null)
+		if ($params && count($params) > 0) {
+			$paramsString = $this->buildPostBody($params);
+			$url .= ("?".$paramsString);
+		}
+
+		if ($headers) {
 			curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+		}
 
 		$response = curl_exec($curl);
 
-		if(curl_errno($curl) != 0) {
+		if (curl_errno($curl) != 0) {
 			throw new Exception("CURL internal error - ".curl_error($curl));
 		}
 
 		$returnCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-		if($returnCode != 200) {
+		if ($returnCode != 200) {
 			throw new Exception("HTTP error code ".$returnCode. " for URL '".$url."'. Response body: ".substr($response, 0, 120));
 		}
  
@@ -50,7 +56,7 @@ class SimpleHTTP {
 		return $response;		
 	}
 
-	public static function post($url, array $data, $auth = null, 
+	public static function post($url, $data, $auth = null, 
 		array $headers = null, $useragent = "NowPlaying(Mozilla Compatible)"
 	) {
 		$curl = curl_init();
@@ -60,37 +66,47 @@ class SimpleHTTP {
 		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
 		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
-		if($auth != null && is_array($auth)) {
+		if ($auth != null && is_array($auth)) {
 			curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 			curl_setopt($curl, CURLOPT_USERPWD, $auth["username"].":".$auth["password"]);
 		}
 
-		if($headers != null) {
+		if ($headers != null) {
 			curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 		}
 
-		$datafields = [];
-		foreach($data as $key => $value) {
-			$datafields[] = ($key."=".urlencode($value));
-		}
-		$datastring = join('&', datafields);
+		$datastring = $this->buildPostBody($data);
 
 		curl_setopt($curl, CURLOPT_POST, count($data));
 		curl_setopt($curl, CURLOPT_POSTFIELDS, $datastring);
 
 		$response = curl_exec($curl);
 
-		if(curl_errno($curl) != 0) {
+		if (curl_errno($curl) != 0) {
 			throw new Exception("CURL error : ".curl_error($curl));
 		}
 
 		$returnCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-		if($returnCode != 200) {
+		if ($returnCode != 200) {
 			throw new Exception("HTTP error code ".$returnCode. " for URL '".$url."'. Response body: ".substr($response, 0, 120));
 		}
 
 		curl_close($curl);
 		return $response;
+	}
+
+	private static function buildPostBody(array $data) {
+		// array behaviour: transform array into a form data string
+		if (is_array($data)) {
+			$datafields = [];
+			foreach($data as $key => $value) {
+				$datafields[] = ($key."=".urlencode($value));
+			}
+			return join('&', datafields);
+		}
+
+		// default behaviour: return string
+		return $data;
 	}
 }
 
