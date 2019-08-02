c96b1d4c5b4893caa19f1e3e017c2748bf2d371f
Stéphane Lepin <stephane.lepin@gmail.com>
2019-08-02 22:31:05 +0200

    Spotify: refactoring

--- lib/DataProvider/Spotify.php
+++ lib/DataProvider/Spotify.php
@@ -20,69 +20,73 @@ License along with this library. If not, see <https://www.gnu.org/licenses/>
 class DataProvider_Spotify implements DataProviderInterface {
 	protected $endpoint = "https://api.spotify.com";
 	protected $track;
-  protected $config;
+	protected $config;
 
 	public function __construct($artist, $title, $config) {
-    $this->config = $config;
-    
-    if($this->config == null 
-      || $this->config['client_id'] == null
-      || $this->config['client_secret'] == null)
-    {
-      throw new Exception("incorrect Spotify configuration");
-    }
+		$this->config = $config;
 
-    $this->track = $this->getTrack($artist, $title);
+		if($this->config == null 
+			|| $this->config['client_id'] == null
+			|| $this->config['client_secret'] == null)
+		{
+			throw new Exception("incorrect Spotify configuration");
+		}
+
+		$this->track = $this->getTrack($artist, $title);
 	}
 
-  public function getCover() {
-  	if($this->track != null) {
-      return $this->track['album']['images'][1]['url'];
-    } else {
-      throw new Exception('No cover art found');
-    }
-  }
+	public function getCover() {
+		if(!$this->track) {
+			throw new Exception('No cover art found');
+		}
+		return $this->track['album']['images'][1]['url'];
+	}
 
-  public function getPreview() {
-    if($this->track != null) {
-    	return $this->track['preview_url'];
-    } else {
-  		throw new Exception('No preview found');
-    }
-  }
+	public function getPreview() {
+		if (!$this->track) {
+			throw new Exception('No preview found');
+		}
+		return $this->track['preview_url'];
+	}
 
-  protected function getTrack($artist, $title) {
-    $url = $this->endpoint;
+	protected function getTrack($artist, $title) {
+		$query = urlencode("artist:\"".$artist."\" ".$title);
 
-    $query = urlencode("artist:\"".$artist."\" ".$title);
+		$access_token = $this->authenticate(
+			$this->config['client_id'], 
+			$this->config['client_secret']);
 
-    $access_token = $this->authenticate(
-      $this->config['client_id'], 
-      $this->config['client_secret']);
+		$data = SimpleHTTP::get(
+			$this->endpoint."/v1/search",
+			[
+				"type" => "track",
+				"q" => $query
+			],
+			null,
+			[ 'Authorization: Bearer '.$access_token ]
+		);
 
-    $data = SimpleHTTP::get(
-      $this->endpoint."/v1/search?type=track&q=".$query,
-      null, array('Authorization: Bearer '.$access_token));
+		$data = json_decode($data, true);
 
-    $data = json_decode($data, true);
+		return $data['tracks']['items'][0];
+	}
 
-    return $data['tracks']['items'][0];
-  }
+	protected function authenticate($client_id, $client_secret) {
+		$resp = SimpleHTTP::post(
+			"https://accounts.spotify.com/api/token",
+			null,
+			array('grant_type' => 'client_credentials'),
+			array('username' => $client_id, 'password' => $client_secret)
+		);
 
-  protected function authenticate($client_id, $client_secret) {
-    $resp = SimpleHTTP::post(
-      "https://accounts.spotify.com/api/token",
-      array('grant_type' => 'client_credentials'),
-      array('username' => $client_id, 'password' => $client_secret)
-    );
+		$resp = json_decode($resp, true);
 
-    $resp = json_decode($resp, true);
+		if (!$resp['access_token']) {
+			throw new Exception('Spotify auth failed');
+		}
 
-    if($resp['access_token'] != null)
-      return $resp['access_token'];
-    else
-      throw new Exception('Spotify auth failed');
-  }
+		return $resp['access_token'];      
+	}
 }
 
 ?>
